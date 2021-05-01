cedc54e6398b9c93dd5f94fb88e6f5a22c5994ae
Sadzka <sirsadzka@gmail.com>
2021-05-01 18:30:40 +0200

    packets and login

--- Server/server.py
+++ Server/server.py
@@ -1,27 +1,71 @@
 import threading
+import time
 
-from UdpSocket import UdpSocket;
+from UdpSocket import UdpSocket
+from database import Connection
+from decimal import Decimal
+from packet import Packet
+from packetTypes import PacketType
 
 class Server:
 
-    def __listen__(self):
+    def __listen(self):
         print("Listening...")
+        cursor = Connection().getInstance()
         while True:
-            data, clientIP, clientPort = self.incoming.receive();
-            print("Received from " + clientIP + ":" + str(clientPort))
-            print("\t", data)
+            data, clientInfo = self.incoming.receive();
+            clientIP, clientPort = clientInfo
             
-            if data == b'ping':
-                self.outgoing.send('pong', client[0], client[1])
+            print("DEBUG: Received from " + clientIP + ":" + str(clientPort))
+            print("DEBUG: ", data)
+            
+            
+            packet = Packet(raw_data = data)
+            type = packet.get_type()
+            
+            print("DEBUG: Type ->", type)
+            
+            if type == PacketType.MESSAGE:
+                print("message: ", packet.get())
+                
+            if type == PacketType.PING:
+                outgoing_packet = Packet(PacketType.PING)
+                outgoing_packet.add("pong")
+                self.outgoing.send(outgoing_packet, clientIP, clientPort)
+                
+            if type == PacketType.LOGIN:
+                outgoing_packet = Packet(PacketType.LOGIN)
+                
+                login = packet.get();
+                password = packet.get();
+                print("DEBUG: Login ->", login)
+                print("DEBUG: Password ->", password)
+                
+                querry = "SELECT * FROM `users` WHERE `username`=? AND `password`=?";
+                cursor.execute(querry, (login, password))
+                
+                users = cursor.fetchall()
+                if len(users) == 1:
+                    user = users[0]
+                    print("DEBUG: Login success")
+                    outgoing_packet.add(1)
+                    self.users.append( [user[0], user[1], clientIP, clientPort] )
+                else:
+                    print("DEBUG: Login failed")
+                    outgoing_packet.add(0)
+                
+                self.outgoing.send(outgoing_packet, clientIP, clientPort)
+                
         
     def __init__(self):
         self.incoming = UdpSocket()
         self.incoming.bind(5555)
+        self.users = [] # [ {id}, {username}, {ip}, {port} ]
         
         self.outgoing = UdpSocket()
         self.outgoing.bind(5556)
         
-        self.listening_thread = threading.Thread(target=self.__listen__, args=(), daemon = True)
+        self.listening_thread = threading.Thread(target=self.__listen, args=(), daemon = True)
         # Database
         # cursor = Connection().getInstance()
         
@@ -29,7 +73,7 @@ class Server:
         self.running = True;
         self.listening_thread.start()
         while (True):
-            pass
+            time.sleep(1000)
 
 
 if __name__ == "__main__":
