e8dff78a089d08de593cd00fc112b493b5c79481
Sadzka <sirsadzka@gmail.com>
2021-05-14 17:55:38 +0200

    Update packetTypes.py

--- Network/packetTypes.py
+++ Network/packetTypes.py
@@ -1,7 +1,12 @@
 from enum import Enum
 
 class PacketType(Enum):
-    MESSAGE     =  1
-    PING        =  2
-    LOGIN       =  3
-    REGISTER    =  4
+    MESSAGE             = 1
+    PING                = 2
+    LOGIN               = 3
+    REGISTER            = 4
+    GET_STATS           = 5
+    GET_ITEMS           = 6
+    GET_ITEM_INFO       = 7
+    SAVE_STATS          = 8
+    SAVE_ITEMS          = 9
