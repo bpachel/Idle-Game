dff1499994b7140bd6e023672b1f46cc7d2a5f56
Sadzka <sirsadzka@gmail.com>
2021-05-14 15:33:42 +0200

    fix

--- Network/UserRepository.py
+++ Network/UserRepository.py
@@ -12,6 +12,9 @@ class UserRepository(AbstractRepository):
     def __create_objects_from_result(self, results):
         users = []
 
+        if not results:
+            return None
+
         for result in results:
             user = User()
             user.id         = result[0]
@@ -25,15 +28,15 @@ class UserRepository(AbstractRepository):
         
     def __init__(self, reset=False):
         self.name = "user"
-        self.delete_querry = """DROP TABLE IF EXISTS `user`;"""
+        self.delete_querry = """DROP TABLE IF EXISTS `{}`;""".format(self.name)
         self.create_querry = """
-        CREATE TABLE `user` (
+        CREATE TABLE `{}` (
             `id`                INTEGER PRIMARY KEY AUTOINCREMENT,
             `username`          VARCHAR(32) NOT NULL UNIQUE,
             `password`          VARCHAR(60) NOT NULL,
             `email`             VARCHAR(255) NOT NULL UNIQUE
         );
-        """
+        """.format(self.name)
         super().__init__(reset)
 
     def findAll(self):
@@ -46,7 +49,7 @@ class UserRepository(AbstractRepository):
 
     def findOneBy(self, arr):
         result = self.findBy(arr)
-        if len(result) > 0:
+        if not result is None and len(result) > 0:
             return result[0]
         return None
 
@@ -58,18 +61,25 @@ class UserRepository(AbstractRepository):
 
 if __name__ == "__main__":
     
-    userRepository = UserRepository()
+    userRepository = UserRepository(True)
 
     user = User()
-    user.username = "abcdef"
-    user.password = "gggggg"
-    user.email = "gggggg"
+    user.username = "qwe"
+    user.password = "$2b$12$ud1WZ0Q/nIh6mkdwwJUgReUXsCMPBR8Wf00gmLmYw4d2EhhyyT75W" # 'aaaaa'
+    user.email = "qwe@qwe.qwe"
+    userRepository.add(user)
 
+    user.username = "asd"
+    user.email = "asd@asd.asd"
+    userRepository.add(user)
+
+    user.username = "zxc"
+    user.email = "zxc@zxc.zxc"
     userRepository.add(user)
     
     for user in userRepository.findAll():
         print (user.id, user.username, user.password, user.email)
 
-    user = userRepository.findOneBy({"id" : 4})
+    user = userRepository.findOneBy({"id" : 1})
     print (user.id, user.username, user.password, user.email)
     #print ( list(user.__dict__.keys() ))
