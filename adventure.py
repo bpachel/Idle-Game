6cf645577784d9c834c21474a98c2cf501e42274
niemazaco <64072380+niemazaco@users.noreply.github.com>
2021-05-20 10:32:44 +0200

    Add files via upload

--- adventure.py
+++ adventure.py
@@ -6,7 +6,7 @@ import random
 
 class CurrencyContainer:
     def __init__(self, dif_level, forclass):
-        # todo generowanie treasures
+        # todo generowanie treasures - balans
         temp = ['might_exp', 'cunning_exp', 'psyche_exp', 'lore_exp']
 
         if forclass == 'Challenge':
@@ -27,22 +27,36 @@ class CurrencyContainer:
         if self.treasures.val > Decimal(0):
             self.print_list.append(self.treasures)
 
-    pass
 
+itemNames = ['Weapon1', 'Weapon2']
+itemTypes = ['Weapon', 'Helmet', 'Armor', 'Ring']
 
 class ItemContainer:
-    # przechowywanie itemow do zdobycia i losowanie ich na nagrode
-    # wywolanie ItemContainer(dif_level, 'challenge')
-    # self.waluta = ItemContainer(dif_level, 'adventure')
-    # dif_level [0,0,0,0]
-    pass
+    #lista przedmiotow za ukonczenie przygody. Dla challenge lista bedzie pusta
+    def __init__(self, dif_level, challenges_count):
+        self.items = []
+        amount = random.randint(8, 12) * challenges_count // 50 # 1 item na 4-6 wyzwan
+        for i in range(amount):
+            #losowanie nazwy przedmiotu
+            name = random.choice(itemNames)
+            #losowanie typu przedmiotu
+            type = random.choice(itemTypes)
+
+            #todo balans ???????????
+            #todo atrybuty aktywne wymagane do zalozenia
+
+            #todo bonusy zalozonego itemu - 4 atrybuty aktywne w kolejnosci w liscie dif_level
+            self.items.append(Item(name, type, minimum=[0, 0, 0, 0], m=0, c=0, p=0, l=0))
 
 
 class Reward:
     # generowanie i przechowywanie nagrody za adventure/challenge
-    def __init__(self, diff_level, type):
+    def __init__(self, diff_level, type, challengesCount=0):
         self.waluta = CurrencyContainer(diff_level, type)
-        self.eq = ItemContainer(diff_level, type)
+        self.items = ItemContainer(diff_level, challengesCount)
+
+    def getItems(self):
+        return self.items.items
 
 
 # todo ustawienie parametrów losowania oraz
@@ -66,9 +80,9 @@ class Challenge:  # wyzwanie
         #testowane 4 atrybuty aktywne
         self.cost = [Decimal(random.randrange(50, 100, 1) / 500) * self.difficulty[i] for i in range(4)]
         #piaty atrybut nieodpowiadajacy testowanym atrybutom todo balans
-        self.cost.append(Decimal(random.randrange(50, 100, 1) / 1800) * sum(dif_level))
+        self.cost.append(Decimal(random.randrange(50, 100, 1) / 1800 * sum(dif_level)))
 
-    '''true if completed challenge, false if not'''
+    '''return true if completed challenge, false if not'''
     def onClock(self, bohater):
         temp_list = [bohater.passive[i].val - self.cost[i] for i in range(5)]
         for i in temp_list:
@@ -94,7 +108,7 @@ class Adventure:  # przygoda
         self.challenges = [Challenge(diff_level) for i in range(self.amount)]
         self.challenge_index = 0
         self.in_action = False
-        self.reward = Reward(diff_level, 'Adventure')
+        self.reward = Reward(diff_level, 'Adventure', self.amount)
         # todo pasek postepu przygody - gdzie?
         # todo pasek postepu danego wyzwania - gdzie?
 
@@ -105,23 +119,22 @@ class Adventure:  # przygoda
         self.in_action = False
 
     '''Zwraca bool czy przygoda zostala ukonczona. 
-    Zwraca blad ObozException jezeli trzeba zastopowac wszystkie przygody, bo wyczerpaly sie atrybuty pasywne.'''
+    rzuca blad CampException jezeli trzeba zastopowac wszystkie przygody, bo wyczerpaly sie atrybuty pasywne.'''
     #todo Wtedy trzeba w nadrzednej metodzie wywolac metode stop dla wszystkich przygod gracza.
     def onClock(self, bohater):
         if self.in_action:
             try:
                 if self.challenges[self.challenge_index].onClock(bohater):
                     bohater.applyReward(self.challenges[self.challenge_index].reward)
-                    if self.challenge_index + 1 == self.amount:
-                        self.challenge_index += 1
+                    self.challenge_index += 1
+                    if self.challenge_index != self.amount:
                         return False
-                    else:
-                        bohater.applyReward(self.reward)
-                        self.in_action = False
-                        return True
+                    bohater.applyReward(self.reward)
+                    self.in_action = False
+                    return True
             except CampException:
                 raise  # do nadrzednej metody
-#todo czy probujemy wykonac nastepne wyzwania w nadziei, ze starczy na nie atr pasywnych, czy pomijamy to?
+#todo w nadrzednej metodzie - czy probujemy wykonac nastepne wyzwania w nadziei, ze starczy na nie atr pasywnych, czy pomijamy to?
             except Exception as e:
                 print(e)
                 print("ADVENTURE EXCEPTION")
